625ea6e47257f9ec4e8f279b0ddadc07d6ce767e
JPedroCMartins <tyr.mars@protonmail.com>
2024-04-13 10:58:30 -0400

    ResourceBundle [Aula-130]

--- maratona_java/src/com/aprendendo/javacore/t_resource_bundle/test/ResourceBundleTest01.java
+++ maratona_java/src/com/aprendendo/javacore/t_resource_bundle/test/ResourceBundleTest01.java
@@ -4,11 +4,17 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ResourceBundleTest01 {
-	public static void main(String[] args) {
-		ResourceBundle bundle = ResourceBundle.getBundle("message", new Locale("pt", "BR"));
-		System.out.println(bundle.getString("hello"));
-		
-		bundle = ResourceBundle.getBundle("message", new Locale("en", "US"));
-		System.out.println(bundle.getString("hello"));
-	}
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        ResourceBundle bundle = ResourceBundle.getBundle("message", new Locale("en", "US"));
+        
+        System.out.println(bundle.getString("hello"));
+        System.out.println(bundle.getString("good.morning"));
+
+        ResourceBundle bundle_pt = ResourceBundle.getBundle("message", new Locale("pt", "BR"));
+        
+        System.out.println(bundle_pt.getString("hello"));
+        System.out.println(bundle_pt.getString("good.morning"));
+        //AULA 130
+    }
 }
